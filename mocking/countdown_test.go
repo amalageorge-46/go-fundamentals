25c64d0550f8032c9a0f86fe167ce0927267ef76
amalageorge-46 <amala.george@waybeo.com>
2026-02-05 12:34:58 +0530

    update countdown func to print 3,2,1,Go!

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -8,10 +8,13 @@ import (
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
 
-	countdown(buffer)
+	Countdown(buffer)
 
 	got := buffer.String()
-	want := "3"
+	want := `3
+2
+1
+Go!`
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)

9b0abb0586e8585e36a268579692bb21fec1d680
amalageorge-46 <amala.george@waybeo.com>
2026-02-05 17:44:42 +0530

    Extending Sleeper to be configurable

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -14,6 +14,11 @@ type Sleeper interface {
 	Sleep()
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
 type DefaultSleeper struct{}
 
 func (d *DefaultSleeper) Sleep() {
@@ -24,10 +29,14 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
-
 	fmt.Fprint(out, finalWord)
 }
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }

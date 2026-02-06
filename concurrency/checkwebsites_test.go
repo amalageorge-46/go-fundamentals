2137a048491696b203045a227b78dc5f364a1f08
amalageorge-46 <amala.george@waybeo.com>
2026-02-06 17:55:28 +0530

    introduce goroutine and channels

--- concurrency/checkwebsites_test.go
+++ concurrency/checkwebsites_test.go
@@ -1,22 +1,33 @@
 package concurrency
 
 import (
+	"reflect"
 	"testing"
-	"time"
 )
 
-func slowStubWebsiteChecker(_ string) bool {
-	time.Sleep(20 * time.Millisecond)
+func mockWebsiteChecker(url string) bool {
+	if url == "waat://furhurterwe.geds" {
+		return false
+	}
 	return true
 }
 
-func BenchmarkCheckWebsites(b *testing.B) {
-	urls := make([]string, 100)
-	for i := 0; i < len(urls); i++ {
-		urls[i] = "a url"
+func TestCheckWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
 	}
 
-	for b.Loop() {
-		CheckWebsites(slowStubWebsiteChecker, urls)
+	want := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	got := CheckWebsites(mockWebsiteChecker, websites)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("wanted %v, got %v", want, got)
 	}
 }

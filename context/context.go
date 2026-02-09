be22a54d2f00466d5d2bf98ff57376e70bbb3a9d
amalageorge-46 <amala.george@waybeo.com>
2026-02-09 19:17:14 +0530

    update the function and test to cancel the request

--- context/context.go
+++ context/context.go
@@ -5,14 +5,26 @@ import (
 	"net/http"
 )
 
-// Store fetches data.
 type Store interface {
 	Fetch() string
+	Cancel()
 }
 
-// Server returns a handler for calling Store.
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, store.Fetch())
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
 	}
 }

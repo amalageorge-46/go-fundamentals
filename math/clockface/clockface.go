18723e998c823a509261f00002203f2587ffc41b
amalageorge-46 <amala.george@waybeo.com>
2026-02-10 15:35:11 +0530

    extend the test to cover a few more scenarios

--- math/clockface/clockface.go
+++ math/clockface/clockface.go
@@ -16,6 +16,15 @@ type Point struct {
 func SecondHand(t time.Time) Point {
 	return Point{150, 60}
 }
+
 func secondsInRadians(t time.Time) float64 {
-	return math.Pi
+	return (math.Pi / (30 / (float64(t.Second()))))
+}
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
 }

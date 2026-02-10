36d5321b271418204d16e19b83d4306ddb9a0192
amalageorge-46 <amala.george@waybeo.com>
2026-02-10 14:58:45 +0530

    initialize SecondHand function and unit test

---
+++ math/clockface/clockface_test.go
@@ -0,0 +1,20 @@
+package clockface_test
+
+import (
+	"testing"
+	"time"
+
+	"example.com/hello/math/clockface"
+)
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	// You MUST use the package prefix because you are in clockface_test
+	want := clockface.Point{X: 150, Y: 150 - 90}
+	got := clockface.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("Got %v, wanted %v", got, want)
+	}
+}

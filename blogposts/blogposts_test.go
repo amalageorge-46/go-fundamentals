23a8921ca47f87fdf6185d40cd0ded8403248d6b
amalageorge-46 <amala.george@waybeo.com>
2026-02-10 18:42:08 +0530

    initialize NewPostsFromFS

---
+++ blogposts/blogposts_test.go
@@ -0,0 +1,25 @@
+package blogposts_test
+
+import (
+	"testing"
+	"testing/fstest"
+
+	blogposts "github.com/amalageorge-46/blogposts"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte("hi")},
+		"hello-world2.md": {Data: []byte("hola")},
+	}
+
+	posts, err := blogposts.NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+}
---
+++ blogposts/go.mod
@@ -0,0 +1,3 @@
+module github.com/amalageorge-46/blogposts
+
+go 1.25.5
